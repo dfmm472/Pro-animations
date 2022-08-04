37bb9aee0a4ee36803993f075692cb0628180e25
Daniel <daniel.munoz@wolox.co>
2022-08-04 08:52:18 -0500

    Add page control example and video

---
+++ PageControls/Example/PageControls/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PageControls
+//
+//  Created by Kyle Zaragoza on 8/5/16.
+//  Copyright © 2016 Kyle Zaragoza. All rights reserved.
+//
+
+import UIKit
+import PageControls
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var snakePageControl: SnakePageControl!
+    @IBOutlet weak var filledPageControl: FilledPageControl!
+    @IBOutlet weak var pillPageControl: PillPageControl!
+    @IBOutlet weak var scrollingPageControl: ScrollingPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // Update scroll view content size.
+        let contentSize = CGSize(width: scrollView.bounds.width * 3,
+                                 height: scrollView.bounds.height)
+        scrollView.contentSize = contentSize
+    }
+}
+
+
+// MARK: - Scroll View Delegate
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.bounds.width
+        let progressInPage = scrollView.contentOffset.x - (page * scrollView.bounds.width)
+        let progress = CGFloat(page) + progressInPage
+        snakePageControl.progress = progress
+        filledPageControl.progress = progress
+        pillPageControl.progress = progress
+        scrollingPageControl.progress = progress
+    }
+}
