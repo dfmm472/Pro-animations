3afaaf5f979c68f47e190ae349c86ea87799400b
Daniel <daniel.munoz@wolox.co>
2022-08-04 08:47:13 -0500

    Add Tabman example code and video

---
+++ Tabman/Sources/TabmanTests/Bar/MockBarDataSource.swift
@@ -0,0 +1,18 @@
+//
+//  MockBarDataSource.swift
+//  TabmanTests
+//
+//  Created by Merrick Sapsford on 01/09/2018.
+//  Copyright © 2019 UI At Six. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Tabman
+
+class MockBarDataSource: TMBarDataSource {
+    
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        return TMBarItem(title: "Mock Page \(index)")
+    }
+}
